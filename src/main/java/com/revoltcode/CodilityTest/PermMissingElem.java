bde9f7e9c311b2b0c172b488ef7341d330c49803
Revolt <revolt@localhost.localdomain>
2022-01-22 14:58:47 +0100

    Perm Missing Element

---
+++ src/main/java/com/revoltcode/CodilityTest/PermMissingElem.java
@@ -0,0 +1,29 @@
+package com.revoltcode.CodilityTest;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class PermMissingElem {
+
+	public int getMissingEelement(int[] data) {
+		
+		HashSet<Integer> valueSet = new HashSet();
+		
+		for(int i=1; i<data.length+1; i++) {
+			valueSet.add(i);
+		}
+		
+		for(int i=0; i<data.length; i++) {
+			valueSet.remove((Integer) data[i]);
+		}
+		
+		return valueSet.iterator().next();
+	}
+	
+	public static void main(String[] args) { 
+		
+		PermMissingElem pme = new PermMissingElem();
+		System.out.println(pme.getMissingEelement(new int[] {2,3,1,5}));
+	}
+}
