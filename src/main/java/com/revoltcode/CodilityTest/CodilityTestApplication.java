a12639f214ddee4857ec5e9ff63d43332fa2b821
Revolt <revolt@localhost.localdomain>
2022-01-22 11:30:08 +0100

    first commit

---
+++ src/main/java/com/revoltcode/CodilityTest/CodilityTestApplication.java
@@ -0,0 +1,13 @@
+package com.revoltcode.CodilityTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodilityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodilityTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revoltcode/CodilityTest/CodilityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revoltcode.CodilityTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodilityTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
