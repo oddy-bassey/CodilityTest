6b0e5e7db1ffb8f95b3ca690b5bd52e70daaa419
Revolt <revolt@localhost.localdomain>
2022-01-24 11:45:19 +0100

    Frog River One

---
+++ src/main/java/com/revoltcode/CodilityTest/FrogRiverOne.java
@@ -0,0 +1,31 @@
+package com.revoltcode.CodilityTest;
+
+import java.util.HashSet;
+
+public class FrogRiverOne {
+
+	public int solution(int x, int[] array) {
+		
+		HashSet<Integer> valueSet = new HashSet<>();
+		for (int i = 1; i <= x; i++) {
+			valueSet.add(i);
+		}
+		
+		for (int i = 0; i < array.length; i++) {
+			if(valueSet.remove(array[i])) {
+				if(valueSet.isEmpty()) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		FrogRiverOne fro = new FrogRiverOne();
+		
+		System.out.println(fro.solution(5, new int[] {1,3,1,4,2,3,5,4}));
+
+		System.out.println(fro.solution(1, new int[] {1}));
+	}
+}
