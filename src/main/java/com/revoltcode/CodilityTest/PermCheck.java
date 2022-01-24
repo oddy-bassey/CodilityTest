8c254d714d8730b7bb6d1762fda7ebb88151b6f7
Revolt <revolt@localhost.localdomain>
2022-01-24 17:34:29 +0100

    Perm Check

---
+++ src/main/java/com/revoltcode/CodilityTest/PermCheck.java
@@ -0,0 +1,29 @@
+package com.revoltcode.CodilityTest;
+
+import java.util.HashSet;
+
+public class PermCheck {
+
+	public int solution(int[] data) {
+	
+		HashSet< Integer> valueSet = new HashSet<>();
+		
+		for (int i = 0; i < data.length; i++) {
+			valueSet.add(i+1);
+		}
+		
+		for (int x : data) {
+			if(!valueSet.contains(x)) {
+				return 0;
+			}else {
+				valueSet.remove(new Integer(x));
+			}
+		}
+		return valueSet.isEmpty()?  1 : 0 ;
+	}
+	public static void main(String[] args) {
+		
+		PermCheck pm = new PermCheck();
+		System.out.println(pm.solution(new int[] {1}));
+	}
+}
