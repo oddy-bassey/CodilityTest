fca0e1603a3d02ced6169d0e709ccc8ec0a5b444
Revolt <revolt@localhost.localdomain>
2022-01-24 14:45:20 +0100

    Max Counters

---
+++ src/main/java/com/revoltcode/CodilityTest/MaxCounters.java
@@ -0,0 +1,43 @@
+package com.revoltcode.CodilityTest;
+
+public class MaxCounters {
+
+	public int[] solution(int n, int[] array) {
+		
+		int maxValue = 0, minValue = 0;
+		int[] counters = new int[n];
+		
+		for (int i = 0; i < array.length; i++) {
+			
+			int operation = array[i];
+			
+			if(operation == n+1) {
+				// max value operation
+				minValue = maxValue;
+
+			}else {
+				// increment operation
+				
+				operation--;
+				counters[operation] = Math.max(counters[operation]+1, minValue+1);
+				maxValue = Math.max(maxValue, counters[operation]);
+			}
+		}
+		
+		for (int i = 0; i < counters.length; i++) {
+			counters[i] = Math.max(counters[i], minValue);
+		}
+		
+		return counters;
+	}
+	
+	public static void main(String[] args) {
+		
+		MaxCounters maxCounters = new MaxCounters();
+		
+		int[] result = maxCounters.solution(5, new int[] {3,4,4,6,1,4,4});
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(result[i]+" ");
+		}
+	}
+}
