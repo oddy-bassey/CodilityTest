0b9181f4c89afc1edcab5de143a8edc0562d1496
oddy-bassey <oddybassey@gmail.com>
2022-07-31 12:48:38 +0100

    Peaks (unsolved)

---
+++ src/main/java/com/revoltcode/CodilityTest/Peaks.java
@@ -0,0 +1,13 @@
+package com.revoltcode.CodilityTest;
+
+public class Peaks {
+
+    public int solution(int[] A) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Peaks().solution(new int[]{1,5,3,4,3,4,1,2,3,4,6,2}));
+    }
+}
