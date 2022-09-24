c7a543fc4835f8be55908411c22d31d26e05e5ac
oddy-bassey <oddybassey@gmail.com>
2022-09-24 01:54:12 +0100

    MinMaxDivision not solved

---
+++ src/main/java/com/revoltcode/CodilityTest/MinMaxDivision.java
@@ -0,0 +1,13 @@
+package com.revoltcode.CodilityTest;
+
+public class MinMaxDivision {
+
+    public int solution(int K, int M, int[] A) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MaxProfit().solution(new int[]{1,5,1,2,2,2}));
+    }
+}
